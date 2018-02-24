7c80850af680b7db999346a0bdbba1af6f53a559
Jonah Meijers <jonahmeijers97@gmail.com>
2018-02-24 15:37:13 +0100

    added route, asset and 404 fallback handling

--- index.js
+++ index.js
@@ -1,9 +1,31 @@
 const http = require('http')
+const fs = require('fs')
+const path = require('path')
+const mt = require('mime-types')
 
-const onrequest = (req, res) => {
-    res.statusCode = 200
-    res.setHeader('Content-Type', 'text/html')
-    res.end(`<h1>${req.url}</h1>\n`)
+const onnotfound = (url, res) => {
+    res.statusCode = 404
+    if (mt.lookup(url) == 'text/html')
+        fs.readFile(path.join(__dirname, '/404.html'), (err, buf) => {
+            if (err) return 404
+            buf.toString()
+            res.end(buf)
+        })
+    else res.end()
+}
+
+const onrequest = async (req, res) => {
+    fs.readFile(path.join(__dirname, req.url), (err, buf) => {
+        if (err) {
+            onnotfound(req.url, res)
+            return
+        }
+        buf.toString()
+        
+        res.statusCode = 200
+        res.setHeader('Content-Type', mt.lookup(req.url))
+        res.end(buf)
+    })
 }
 
 http.createServer(onrequest).listen(8000)
